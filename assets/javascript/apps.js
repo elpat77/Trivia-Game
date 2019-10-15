7c73c240a3b9fb0251e3230303b499b2e3ae88b8
elpat7 <aguilarpat7@gmail.com>
2019-10-15 12:45:58 -0700

    updating html format/names

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,31 +1,35 @@
-var number = 100;
+$(document).ready(function () {
+    console.log('hi');
+});
+
+var number;
 var intervalId;
 
 function run() {
+    number = 10;
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
+}
+function stop() {
+    clearInterval(intervalId);
 }
 
 //  The decrement function.
 function decrement() {
-
     //  Decrease number by one.
     number--;
-
     //  Show the number in the #show-number tag.
     $("#show-number").html("<h2>" + " Time Remaining: " + number + "</h2>");
-
-
     //  Once number hits zero...
     if (number === 0) {
-
         //  ...run the stop function.
         stop();
-
         //  Alert the user that time is up.
-        alert("Time Up!");
+        alert("Time is Up!");
     }
 }
-run()
+run();
 
-// $(document).ready(function () {
+
+// function updateQuiz
+
