d1e7d7886d3ec52d16961536c01fbd9ecee30061
elpat7 <aguilarpat7@gmail.com>
2019-10-17 15:10:39 -0700

    saving progress, working update questions and answers functions

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    console.log('hi');
     updateQuestions();
     updateAnswers();
 });
@@ -77,9 +76,9 @@ var myQuestions = [
 ]
 
 console.log('my questions', myQuestions);
-console.log(myQuestions.forEach((questions) => console.log('your questions ', questions.question)));
-console.log(myQuestions.forEach((answers) => console.log('your answers', answers.answer)));
-console.log(myQuestions.forEach((correct) => console.log('correct ', correct.correctAnswer)));
+// console.log(myQuestions.forEach((questions) => console.log('your questions ', questions.question)));
+// console.log(myQuestions.forEach((answers) => console.log('your answers', answers.answer)));
+// console.log(myQuestions.forEach((correct) => console.log('correct ', correct.correctAnswer)));
 
 // console.log(myQuestions.question);
 // console.log(myQuestions.answer);
@@ -90,20 +89,17 @@ console.log(myQuestions.forEach((correct) => console.log('correct ', correct.cor
 function updateQuestions() {
     for (let index = 0; index < myQuestions.length; index++) {
         $('#question' + (index + 1)).html(myQuestions[index].question);
+        console.log('all questions' + myQuestions[index].question);
+        ;
 
     }
 }
 
-
-
 function updateAnswers() {
-    // for (let index = 0; index < myQuestions.length; index++) {
-    //     $('#answers' + (index + 1)).html(myQuestions[index].question);
-
-    $('label[for=answer1]').html(myQuestions[0].answer.a);
-    $('label[for=answer2]').html(myQuestions[0].answer.b);
-    $('label[for=answer3]').html(myQuestions[0].answer.c);
-    $('label[for=answer4]').html(myQuestions[0].answer.d);
+    $('label[for=q1answer1]').html(myQuestions[0].answer.a);
+    $('label[for=q1answer2]').html(myQuestions[0].answer.b);
+    $('label[for=q1answer3]').html(myQuestions[0].answer.c);
+    $('label[for=q1answer4]').html(myQuestions[0].answer.d);
     $('label[for=q2answer1]').html(myQuestions[1].answer.a);
     $('label[for=q2answer2]').html(myQuestions[1].answer.b);
     $('label[for=q2answer3]').html(myQuestions[1].answer.c);
@@ -117,6 +113,26 @@ function updateAnswers() {
     $('label[for=q4answer3]').html(myQuestions[3].answer.c);
     $('label[for=q4answer4]').html(myQuestions[3].answer.d);
 }
+
+
+
+    // for (let index = 0; index < myQuestions.length; index++) {
+    //     // $('#answers').html(myQuestions.answer);
+    //     console.log('answer' + (myQuestions[index].answer));
+    //     // console.log(index + 1)
+
+    //     // console.log('answer a ' + myQuestions[index].answer.a);
+    //     // console.log('answer b ' + myQuestions[index].answer.b);
+    //     // console.log('answer c ' + myQuestions[index].answer.c);
+    //     // console.log('answer d ' + myQuestions[index].answer.d);
+    //     // $('label[for=answer]' + (index + 1)).html(myQuestions[index].answer);
+    //     // $('label[for=answer2]').html(myQuestions[index].answer.b);
+    //     // $('label[for=answer3]').html(myQuestions[index].answer.c);
+    //     // $('label[for=answer4]').html(myQuestions[index].answer.d);
+
+        //increment the target attribute , either by name or id incremnet instead of label[for=answer1]
+
+
 
 
 
